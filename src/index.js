03f21360645b34520bd6b473b0b820f4611d712b
rickycodes <ricky.miller@gmail.com>
2018-06-22 01:16:00 -0400

    events!

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ const { channels, server, botName } = require('./config')
 const handle = require('./say')
 const client = new irc.Client(server, botName, { channels })
 
+const events = [
+  'issues.opened', 'issues.closed', 'issues.reopened',
+  'pull_request.opened', 'pull_request.closed', 'pull_request.reopened'
+]
+
 module.exports = bot => {
-  bot.on(
-    [
-      'issues.opened', 'issues.closed', 'issues.reopened',
-      'pull_request.opened', 'pull_request.closed', 'pull_request.reopened'
-    ],
-    handle.bind(null, client)
-  )
+  bot.on(events, handle.bind(null, client))
 }
